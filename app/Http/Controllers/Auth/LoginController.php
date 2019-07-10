bafc35f737df9e107a84632cc961d18b0f66200b
DiamondK9 <tongquansinh13@gmail.com>
2019-07-10 21:07:34 +0700

    Update LoginController.php
    Trying to fix the problems:
    1. Successful user login still remained in dashboard.
    2. Admin user still : These credentials do not match with our records . Guessing-> somehow the login function doesn't regcognised there is admin user in user table but only check the user table in sql.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,8 +27,12 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'public/home';
-
+    protected $redirectTo = '/home';
+    // protected function authenticated() {
+    // if (Auth::check()) {
+    //     return redirect()->route('../home');
+    // }
+//}
     /**
      * Create a new controller instance.
      *
